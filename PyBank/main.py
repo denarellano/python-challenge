b7987550eb1e9a39e620cab6fd899030b97f168c
Denise <denisetarellano@gmail.com>
2018-07-16 23:44:47 -0700

    financial analysis

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+import os
+import csv
+
+budget_data = os.path.join('budget_data.csv')
+
+# Variables to Hold Data
+num_months = 0
+profit_total = 0
+previous_profit = 0
+total_change = 0
+change = 0
+previous_value = 0
+max_increase_profit = 0
+max_increase_date = ''
+max_decrease_profit = 0
+max_decrease_date = ''
+
+# Open File: with open(filename, 'r') as csvfile:
+with open(budget_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+    for row in csvreader:
+        num_months += 1
+        profit_total = int(row[1])
+        change = profit_total - previous_value
+
+        if num_months > 1:
+            total_change += change
+
+        if change > max_increase_profit:
+            max_increase_date = row[0]
+            max_increase_profit = change
+        
+        if change < max_decrease_profit:
+            max_decrease_date = row[0]
+            max_decrease_profit = change
+
+    avg_change = total_change /(num_months-1)
+
+# Set output file
+output_file = os.path.join('Financial_Analysis.txt')
+
+# Open ouptut file
+with open(output_file, 'w') as data:
+    writer = csv.writer(data, delimiter='\n')
+    writer.writerow(["Financial Analysis",
+                "-" * 30, 
+                f"Total Months: {num_months}",
+                f"Total: ${profit_total}",
+                f"Average Change: ${avg_change}",
+                f"max Decrease in Profits: {max_decrease_date} (${max_decrease_profit})",
+                f"Greates Increase in Profits: {max_increase_date} (${max_increase_profit})",
+                ])
+
+with open(output_file, 'r') as dataprint:
+    print_data = csv.reader(dataprint, delimiter='\n')
+    print('\n')
+    for line in print_data:
+        print(line[0])
+    print('\n')
---
+++ PyPoll/main.py
@@ -0,0 +1,64 @@
+#create a Python script that analyzes the votes and calculates each of the following:
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+
+import os
+import csv
+
+election_data = os.path.join('election_data.csv')
+
+total_votes = 0
+num_votes = {}
+candidates = []
+percent_vote = 0
+winnervalue = 0
+winner = ''
+
+with open(election_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+    for row in csvreader:
+        total_votes += 1
+
+        candidates = row[2]
+
+        if candidates in num_votes:
+            num_votes[candidates] += 1
+        else: 
+            num_votes[candidates] = 1
+        
+    for k, v in num_votes.items():
+        percent_vote = '{0:3f}%'.format((v/total_votes) * 100)
+
+        if v > winnervalue:
+            winnervalue = v
+            winner = k
+
+# Set output file
+output_file = os.path.join('Election_Results.txt')
+
+# Open ouptut file
+with open(output_file, 'w') as data:
+    writer = csv.writer(data, delimiter='\n')
+    writer.writerow(["Election Results",
+                "-" * 20, 
+                f"Total Votes: {total_votes}",
+                "-" * 20,
+                f"Winner: {winner}",
+                "-" * 20,
+                ])
+
+with open(output_file, 'r') as dataprint:
+    print_data = csv.reader(dataprint, delimiter='\n')
+    print('\n')
+    for line in print_data:
+        print(line[0])
+    print('\n')
+
+
+
